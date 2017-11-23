1deaebea1e6376b602ba5ac16963c91ed44d1b6d
Milly <millydack@gmail.com>
2017-11-23 12:05:29 +0100

    Add files via upload

---
+++ Entregas/Entregable4/flujoNumeros.js
@@ -0,0 +1,45 @@
+class FlujoNumeros {
+    constructor() {
+        this.numeros = [6, 1, 4, 3, 10, 9, 8];
+    }
+    
+    siguienteNumero(f) {
+        setTimeout(() => {
+          let result = this.numeros.shift();
+          f(result);
+        }, 100);
+    }
+}
+
+
+/**
+ * Imprime la suma de los dos primeros números del flujo pasado como parámetro.
+ */
+function sumaDosLog(flujo) {
+    /* Implementar */    
+}
+
+/**
+ * Llama a la función f con la suma de los dos primeros números del flujo pasado como parámetro.
+ */
+function sumaDos(flujo, f) {
+    /* Implementar */
+}
+
+/**
+ * Llama a la función f con la suma de todos los números del flujo pasado como parámetro
+ */
+function sumaTodo(flujo, f) {
+    /* Implementar */    
+}
+
+
+
+/* NO MODIFICAR A PARTIR DE AQUÍ */
+
+module.exports = {
+    FlujoNumeros: FlujoNumeros,
+    sumaDosLog: sumaDosLog,
+    sumaDos: sumaDos,
+    sumaTodo: sumaTodo
+}
