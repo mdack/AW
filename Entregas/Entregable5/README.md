5d14508434168761460c8c085e70564b922d8454
Milly <millydack@gmail.com>
2017-11-24 01:24:02 +0100

    Add files via upload

---
+++ Entregas/Entregable5/README.md
@@ -0,0 +1,5 @@
+Antes de empezar:
+
+* Crea una BD vacía llamada `tareas` mediante MySQL/phpMyAdmin. Importa el fichero `tareas.sql`.
+* Ejecuta `npm install` para reconstruir la carpeta `node_modules` con las dependencias del proyecto (en particular, al librería `mysql`).
+* Modifica el fichero `config.js` con los datos de acceso a tu SGBD.
---
+++ Entregas/Entregable5/dao_tasks.js
@@ -0,0 +1,168 @@
+/**
+* Milagros del Rocío Peña Quineche
+* Juan Gómez-Martinho González
+*/
+"use strict";
+
+
+/**
+ * Proporciona operaciones para la gestión de tareas
+ * en la base de datos.
+ */
+class DAOTasks {
+    /**
+     * Inicializa el DAO de tareas.
+     * 
+     * @param {Pool} pool Pool de conexiones MySQL. Todas las operaciones
+     *                    sobre la BD se realizarán sobre este pool.
+     */
+    constructor(pool) {
+        this.pool = pool;
+    }
+
+
+    /**
+     * Devuelve todas las tareas de un determinado usuario.
+     * 
+     * Este método devolverá (de manera asíncrona) un array
+     * con las tareas de dicho usuario. Cada tarea debe tener cuatro
+     * atributos: id, text, done y tags. El primero es numérico, el segundo
+     * una cadena, el tercero un booleano, y el cuarto un array de cadenas.
+     * 
+     * La función callback ha de tener dos parámetros: un objeto
+     * de tipo Error (si se produce, o null en caso contrario), y
+     * la lista de tareas (o undefined, en caso de error).
+     * 
+     * @param {string} email Identificador del usuario.
+     * @param {function} callback Función callback.
+     */
+    getAllTasks(email, callback) {
+
+       this.pool.getConnection((err, conexion)=>{
+        if(err){
+            callback(err);
+        }
+
+        const sql ="SELECT t.id, t.text, t.done, g.tag FROM task t LEFT JOIN tag g ON t.id = g.taskId WHERE t.user = ? ORDER BY t.id";
+
+        conexion.query(sql, [email], function(err, tasks){
+            if(err){
+                callback(err);
+            }else{
+                let list = [];
+                let tarea = tasks[0];
+                let tags = [];
+                
+                tasks.forEach(element => {
+                    if(element.id !== tarea.id){
+                        tarea.tag = tags;
+                        list.push(tarea);
+                        tags = [];
+                        tarea = element;
+                    }
+                    if(element.tag !== null){
+                       tags.push(element.tag);
+                    } 
+                });
+                tarea.tag = tags;
+                list.push(tarea);
+                conexion.release();
+                callback(err, list);
+            }
+            });
+        });
+    }
+
+    /**
+     * Inserta una tarea asociada a un usuario.
+     * 
+     * Se supone que la tarea a insertar es un objeto con, al menos,
+     * dos atributos: text y tags. El primero de ellos es un string con
+     * el texto de la tarea, y el segundo de ellos es un array de cadenas.
+     * 
+     * Tras la inserción se llamará a la función callback, pasándole el objeto
+     * Error, si se produjo alguno durante la inserción, o null en caso contrario.
+     * 
+     * @param {string} email Identificador del usuario
+     * @param {object} task Tarea a insertar
+     * @param {function} callback Función callback que será llamada tras la inserción
+     */
+    insertTask(email, task, callback) {
+        this.pool.getConnection((err, conexion)=>{
+            if(err){
+                callback(err);
+            }
+            const sql1 ="INSERT INTO task (user, text, done) VALUES (?, ?, ?)";
+    
+            conexion.query(sql1, [email, task.text, task.done], function(err, result){
+                if(err){
+                    callback(err);
+                }else{         
+                    const sql2="INSERT INTO tag (taskId, tag) VALUES (?,?)"
+                    for(var i = 0; i<task.tags.length; i++){
+                    conexion.query(sql2, [result.insertId, task.tags[i]], function(err, result){
+                        if(err){
+                            callback(err);
+                        }
+                    });
+                    }
+                    conexion.release();
+                     callback(err);
+                }
+            });
+            });
+        
+    }
+
+    /**
+     * Marca la tarea indicada como realizada, estableciendo
+     * la columna 'done' a 'true'.
+     * 
+     * Tras la actualización se llamará a la función callback, pasándole el objeto
+     * Error, si se produjo alguno durante la actualización, o null en caso contrario.
+     * 
+     * @param {object} idTask Identificador de la tarea a modificar
+     * @param {function} callback Función callback que será llamada tras la actualización
+     */
+    markTaskDone(idTask, callback) {
+        this.pool.getConnection((err, connection) => {
+            if (err) { callback(err); return; }
+            connection.query(
+                "UPDATE task SET done = 1 WHERE id = ?",
+                [idTask],
+                (err) => {
+                    connection.release();
+                    callback(err);
+                }
+            );
+        });
+    }
+
+    /**
+     * Elimina todas las tareas asociadas a un usuario dado que tengan
+     * el valor 'true' en la columna 'done'.
+     * 
+     * Tras el borrado se llamará a la función callback, pasándole el objeto
+     * Error, si se produjo alguno durante la actualización, o null en caso contrario.
+     * 
+     * @param {string} email Identificador del usuario
+     * @param {function} callback Función llamada tras el borrado
+     */
+    deleteCompleted(email, callback) {
+        this.pool.getConnection((err, connection) => {
+            if (err) { callback(err); return; }
+            connection.query(
+                "DELETE FROM task WHERE user = ? AND done = 1",
+                [email],
+                (err) => {
+                    connection.release();
+                    callback(err);
+                }
+            );
+        });
+    }
+}
+
+module.exports = {
+    DAOTasks: DAOTasks
+}
